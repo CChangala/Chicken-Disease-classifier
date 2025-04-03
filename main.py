a4adcf1acd1dd1ea1f5fd4056d84b8f7afe0ea20
CChangala <cchangala@umass.edu>
2025-04-03 02:10:10 -0400

    add data ingestion pipeline

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath("src"))
+
+from src.cnn_classifier import logger
+from src.cnn_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
